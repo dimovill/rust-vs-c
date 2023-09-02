448a2d1cf047e43ad10eebc788e87bdc9f1fc353
Diego Molina Villa <diegomolinavilla@gmail.com>
2023-09-02 18:57:26 +0200

    Intentando Debugger

---
+++ fire/target/debug/deps/fire-6e276b697dd17a78.d
@@ -0,0 +1,5 @@
+/home/diego/rust-vs-c/fire/target/debug/deps/fire-6e276b697dd17a78.rmeta: src/main.rs
+
+/home/diego/rust-vs-c/fire/target/debug/deps/fire-6e276b697dd17a78.d: src/main.rs
+
+src/main.rs:
